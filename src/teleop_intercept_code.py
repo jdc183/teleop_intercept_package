836a8346222a6f1e1da52c3394b2435907f08842
jdc183 <jdc183@case.edu>
2019-09-18 16:13:51 -0400

    Added object avoidance feature.

--- src/teleop_intercept_code.py
+++ src/teleop_intercept_code.py
@@ -2,18 +2,39 @@
 import roslib; roslib.load_manifest("teleop_intercept_package")
 import rospy
 import sys
+import math
+import copy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 global laser
 
 def on_cmd(data):
-	rospy.loginfo("forward velocity: " + str(data.linear.x))
+	print("on_cmd")
+	global pub
+	gk = 0.05
+
+	cmd_vel = copy.copy(data)
+	numLasers = len(laser.ranges)
+
+	for n in range(numLasers):
+		angle = laser.angle_increment*n - laser.angle_min
+		vscalar = gk/(laser.ranges[n]+1)**2
+		cmd_vel.linear.x += vscalar*math.cos(angle)
+		cmd_vel.linear.x += vscalar*math.sin(angle)
+	
+	print(cmd_vel.linear.x)
+	pub.publish(cmd_vel)
 
 def on_lidar(data):
+	global laser
 	rospy.loginfo("Distance to straight: " + str(data.ranges[135]))
+	laser = data
 
 def intercept(robot, steering):
+	print("intercept")
+	global pub
+	laser = LaserScan()
 	pub = rospy.Publisher(str(robot) + "/cmd_vel", Twist, queue_size=10)
 	rospy.init_node("teleop_intercept_node", anonymous=True)
 	telem_sub =  rospy.Subscriber(steering, Twist, on_cmd)
