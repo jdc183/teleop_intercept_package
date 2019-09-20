650849d64e317395828c546b308f990c12067313
jdc183 <jdc183@case.edu>
2019-09-20 13:43:01 -0400

    Finished obstacle avoidance stuffz.

--- src/teleop_intercept_code.py
+++ src/teleop_intercept_code.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+# roslaunch xml file: package, type = executable.py, name = node, elements
 import roslib; roslib.load_manifest("teleop_intercept_package")
 import rospy
 import sys
@@ -8,28 +10,51 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 global laser
+global des_vel
 
 def on_cmd(data):
 	print("on_cmd")
-	global pub
-	gk = 0.05
-
-	cmd_vel = copy.copy(data)
-	numLasers = len(laser.ranges)
-
-	for n in range(numLasers):
-		angle = laser.angle_increment*n - laser.angle_min
-		vscalar = gk/(laser.ranges[n]+1)**2
-		cmd_vel.linear.x += vscalar*math.cos(angle)
-		cmd_vel.linear.x += vscalar*math.sin(angle)
+        global des_vel
 	
-	print(cmd_vel.linear.x)
-	pub.publish(cmd_vel)
+	des_vel = data
 
 def on_lidar(data):
 	global laser
+        global pub
+        global des_vel
+
+        laser = data
+        cmd_vel = copy.deepcopy(des_vel)
+        
+	numLasers = len(laser.ranges)
+        print(des_vel.angular.z)
+        cmd_vel.angular.z = 0.0
+        print(des_vel.angular.z)
+        cmd_vel.linear.x = des_vel.linear.x / 2.0
+        
 	rospy.loginfo("Distance to straight: " + str(data.ranges[135]))
 	laser = data
+	for n in range(int(numLasers)):
+		angle = laser.angle_increment*n + laser.angle_min
+		vscalar = -1/((50*laser.ranges[n])**5+1)
+		cmd_vel.linear.x += 100000*vscalar*math.cos(angle)
+		cmd_vel.angular.z += 2000000*vscalar*angle/(4*angle**6+1)
+                
+
+        cmd_vel.angular.z = cmd_vel.angular.z*abs(des_vel.linear.x) + des_vel.angular.z
+                
+	if cmd_vel.linear.x > 1:
+		cmd_vel.linear.x = 1
+	elif cmd_vel.linear.x < -1:
+		cmd_vel.linear.x = -1
+	if cmd_vel.angular.z > 6:
+		cmd_vel.angular.z = 6
+	elif cmd_vel.angular.z < -6:
+		cmd_vel.angular.z = -6
+
+
+        
+	pub.publish(cmd_vel)
 
 def intercept(robot, steering):
 	print("intercept")
@@ -43,7 +68,7 @@ def intercept(robot, steering):
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
-		print("Not \'nuff args, using defaults")
+		print("Not enough args, using defaults")
 		robot = "robot0"
 		steering = "des_vel"
 	else:
